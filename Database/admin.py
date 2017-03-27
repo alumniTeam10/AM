17a7900e50050bb80f51f09a0dc48ce32b8e0f6e
vinayr <vinuhassan007@gmail.com>
2017-03-26 23:28:51 -0400

    event

--- Database/admin.py
+++ Database/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User,Student,Alumni,Faculty,UserDocumentMap,DocumentType
+from .models import User,Student,Alumni,Faculty,UserDocumentMap,DocumentType,Event,Complaints
 
 # Register your models here.
 admin.site.register(User)
@@ -8,3 +8,5 @@ admin.site.register(Alumni)
 admin.site.register(Faculty)
 admin.site.register(UserDocumentMap)
 admin.site.register(DocumentType)
+admin.site.register(Event)
+admin.site.register(Complaints)
--- admin_module/views.py
+++ admin_module/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from Database.models import Event
 
 # Create your views here.
+
+def post_home(request):
+    event_list=Event.objects.all()
+    for eve in event_list:
+        print eve.event_name
+    event_dict={'event_name':event_list}
+    return render(request,'AlumniManagement/createEvent.html',context=event_dict)
